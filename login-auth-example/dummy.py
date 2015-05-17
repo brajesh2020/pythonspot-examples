097c2bd9b3d9adc7d586cc14cbadd8c996726c24
Michael Ran <theimmortalfish@gmail.com>
2015-05-17 22:00:55 +0800

    fist commit, included 3 example projects

---
+++ login-auth-example/dummy.py
@@ -0,0 +1,26 @@
+# This will put dummy data into your database 'tutorial.db'
+
+import datetime
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from tabledef import *
+
+engine = create_engine('sqlite:///tutorial.db', echo=True)
+
+# create a Session
+Session = sessionmaker(bind=engine)
+session = Session()
+
+user = User("admin","password")
+session.add(user)
+
+user = User("python","python")
+session.add(user)
+
+user = User("jumpiness","python")
+session.add(user)
+
+# commit the record the database
+session.commit()
+
+session.commit()
